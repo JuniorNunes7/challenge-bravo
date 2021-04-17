bf4661671382486d32729f7d6c1ef824b8021fef
Junior Nunes <nunes.junior92@gmail.com>
2021-04-17 08:35:42 -0300

    criando exemplo de teste de API

---
+++ tests/API/CurrencyConvertTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\API;
+
+use Tests\TestCase;
+
+class CurrencyConvertTest extends TestCase
+{
+
+    /**
+     * Test - Currency convert endpoint with right params
+     *
+     * @return void
+     */
+    public function testCurrencyConvertEndpointRightParams()
+    {
+        $this->assertTrue(true);
+    }
+}
